3bcc295e022d6dc345f9d1f9cb714919caf2a71c
SusaneSilva <susaneraphaella.gmail.com>
2021-06-06 23:33:39 -0300

    Exercicio de JavaScript III

--- exercicio-sabado/exercicio-casa/script.js
+++ exercicio-sabado/exercicio-casa/script.js
@@ -1,4 +1,5 @@
 /// Manipulando objetos 
+
 // Dado o seguinte objeto:
 
 const users = [
@@ -16,33 +17,16 @@ const users = [
 
 /*
   Você recebeu uma array de objetos da nova desenvolvedora backend do seu trabalho dos sonhos. Porém, percebeu que não vai precisar de todas essas informações! Por conta disso, será necessário criar um novo objeto apenas com aquilo que será utilizado
-
   Dado o input "users", crie um novo objeto a partir da acima. Ela deverá ter a interface descrita abaixo
-
   interface TableItem {
     fullName: string;
     genres: string;
     score: number;
   }
-
   OUTPUT (o que sai do console)
-
   tableItem = {
     fullName: "Mariana Lopes";
     genres: ["kpop", "rock"];
     score: 153
-
   }
 */
-
-//UMA POSSÍVEL RESPOSTA RESPOSTA
-
-users.map((item) => { //mapear os itens do objeto
-  const tableItem = { //criar uma constante para guardar o novo objeto
-    fullName: `${item.fullName.firstName} ${item.fullName.lastName}`, //template string
-    genres: item.genres.join(' '), //método 'join' junta os elementos em uma string e retorna a string
-    score: item.score 
-  } 
-  console.log(tableItem) //printa no console o novo objeto
-  //opção: return tableItem
-})
